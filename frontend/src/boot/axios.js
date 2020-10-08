a75c035a166796c9a6ac7cd1d5d7f038200a58a3
Shailee Mehta <unknown>
2020-10-08 21:41:00 +0530

    adding login check on route change and api call

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,29 +1,42 @@
 import Vue from "vue";
 import axios from "axios";
 import { getCookie } from "src/services/cookies";
+import { checkIfLoggedIn } from "src/services/login.js";
 
 const axiosInstance = axios.create({
   baseURL: process.env.BASE_URL
 });
 
-axiosInstance.interceptors.request.use(
-  config => {
-    if (config.url !== "/users/login") {
-      const token = getCookie("token");
-      if (token) {
-        config.headers["Authorization"] = "Bearer " + token;
+export default ({ urlPath }) => {
+  axiosInstance.interceptors.request.use(
+    async config => {
+      if (config.url !== "/users/login") {
+        const token = getCookie("token");
+        if (token) {
+          config.headers["Authorization"] = "Bearer " + token;
+        }
       }
+      if (!config.params) {
+        config.params = {};
+      }
+      config.params["_t"] = new Date().getTime();
+
+      if (config.url === "/users/login") {
+        return config;
+      }
+
+      const result = await checkIfLoggedIn();
+      if (!result) {
+        window.location.reload();
+        window.location.href = "/#/login";
+      }
+      return config;
+    },
+    error => {
+      Promise.reject(error);
     }
-    if (!config.params) {
-      config.params = {};
-    }
-    config.params["_t"] = new Date().getTime();
-    return config;
-  },
-  error => {
-    Promise.reject(error);
-  }
-);
+  );
+};
 export const $axios = axiosInstance;
 
 Vue.prototype.$axios = axiosInstance;

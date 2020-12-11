03cb84b93e7128692c4b2dab451f9b07524b97bb
Shailee Mehta <mehtashailee21@gmail.com>
2020-12-11 22:26:56 +0530

    adding calendar chart

--- frontend/src/features/ResidentDetails/services/chart-services.js
+++ frontend/src/features/ResidentDetails/services/chart-services.js
@@ -159,6 +159,141 @@ export const renderFacilitatorTypesChart = (id, facilitatorRoleCounts = []) => {
   });
 };
 
-export const renderCalendarChart= (id, summedActivities)=>{
-  //ToDo:: implement calendar chart
-}
+export const renderCalendarChart = (id, colorClass, summedActivities) => {
+  const width = 960;
+  const height = 400;
+  const cellSize = 15;
+
+  const no_months_in_a_row = Math.floor(width / (cellSize * 5 + 50));
+  const shift_up = cellSize * 3;
+
+  const day = d3.timeFormat("%w"); // day of the week
+  const week = d3.timeFormat("%U"); // week number of the year
+  const month = d3.timeFormat("%m"); // month number
+  const year = d3.timeFormat("%Y");
+  const percent = d3.format(".1%");
+  const format = d3.timeFormat("%Y-%m-%d");
+
+  const minYear = d3.min(
+    summedActivities.map(r => new Date(r.timestamp).getFullYear())
+  );
+  const maxYear = d3.max(
+    summedActivities.map(r => new Date(r.timestamp).getFullYear())
+  );
+
+  d3.selectAll(`.${colorClass}`).remove();
+
+  const svg = d3
+    .select(`#${id}`)
+    .data(d3.range(minYear - 1, maxYear + 1))
+    .enter()
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", `${colorClass}`)
+    .append("g");
+
+  const rect = svg
+    .selectAll(".day")
+    .data(d => d3.timeDays(new Date(d, 0, 1), new Date(d + 1, 0, 1)))
+    .enter()
+    .append("rect")
+    .attr("class", "day")
+    .attr("width", cellSize)
+    .attr("height", cellSize)
+    .attr("x", d => {
+      const month_padding =
+        1.2 * cellSize * 7 * ((month(d) - 1) % no_months_in_a_row);
+      return day(d) * cellSize + month_padding;
+    })
+    .attr("y", d => {
+      const week_diff = week(d) - week(new Date(year(d), month(d) - 1, 1));
+      const row_level = Math.ceil(month(d) / no_months_in_a_row);
+      return (
+        week_diff * cellSize +
+        row_level * cellSize * 8 -
+        cellSize / 2 -
+        shift_up
+      );
+    })
+    .datum(format);
+
+  svg
+    .selectAll(".month-title")
+    .data(function(d) {
+      return d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1));
+    })
+    .enter()
+    .append("text")
+    .text(monthTitle)
+    .attr("x", d => {
+      const month_padding =
+        1.2 * cellSize * 7 * ((month(d) - 1) % no_months_in_a_row);
+      return month_padding;
+    })
+    .attr("y", d => {
+      var week_diff = week(d) - week(new Date(year(d), month(d) - 1, 1));
+      var row_level = Math.ceil(month(d) / no_months_in_a_row);
+      return (
+        week_diff * cellSize + row_level * cellSize * 8 - cellSize - shift_up
+      );
+    })
+    .attr("class", "month-title")
+    .attr("d", monthTitle);
+
+  svg
+    .selectAll(".year-title")
+    .data(d => d3.timeYears(new Date(d, 0, 1), new Date(d + 1, 0, 1)))
+    .enter()
+    .append("text")
+    .text(yearTitle)
+    .attr("x", function(d, i) {
+      return width / 2 - 100;
+    })
+    .attr("y", function(d, i) {
+      return cellSize * 5.5 - shift_up;
+    })
+    .attr("class", "year-title")
+    .attr("d", yearTitle);
+
+  const activities = Array.from(
+    d3.groups(summedActivities, obj => format(obj.timestamp)),
+    ([key, value]) => ({ key, value })
+  );
+
+  const data = activities.reduce(
+    (acc, curr) => ({
+      ...acc,
+      [curr.key]: curr.value.reduce((ac, c) => ac + c.duration, 0)
+    }),
+    {}
+  );
+  const allDurations = Object.values(data);
+  const color = d3
+    .scaleQuantize()
+    .domain([d3.min(allDurations), d3.max(allDurations)])
+    .range(
+      d3.range(11).map(function(d) {
+        return "q" + d + "-11";
+      })
+    );
+
+  rect
+    .filter(d => {
+      return d in data;
+    })
+    .attr("class", function(d) {
+      return "day " + color(data[d]);
+    })
+    .append("title")
+    .text(function(d) {
+      return d + ": " + percent(data[d]);
+    });
+
+  function monthTitle(t0) {
+    return t0.toLocaleString("en-us", { month: "long" });
+  }
+  function yearTitle(t0) {
+    return t0.toString().split(" ")[3];
+  }
+};

934d58d2e0473cce2a1ab1ae02c46e8d93d5fa7b
Shailee Mehta <mehtashailee21@gmail.com>
2020-12-11 22:26:56 +0530

    adding calendar chart

--- frontend/src/features/ResidentDetails/components/ActivitiesCalendar.vue
+++ frontend/src/features/ResidentDetails/components/ActivitiesCalendar.vue
@@ -1,9 +1,10 @@
 <template>
-  <svg class="fill-height full-width" :id="calendarId"></svg>
+  <div class="fit" :id="calendarId"></div>
 </template>
 <script>
 import { getDaywiseActivityDurationApi } from "../services/resident-services";
 import { renderCalendarChart } from "../services/chart-services";
+import * as d3 from "d3";
 
 export default {
   props: { residentId: { type: String, required: true } },
@@ -18,16 +19,67 @@ export default {
     this.renderCalendar();
   },
 
+  beforeDestroy() {
+    d3.selectAll(".RdYlGn").remove();
+  },
+
   methods: {
     async renderCalendar() {
       const data = await getDaywiseActivityDurationApi(this.residentId);
-      renderCalendarChart(this.calendarId, data);
+      renderCalendarChart(this.calendarId, "RdYlGn", data);
     },
   },
 };
 </script>
-<style lang="scss" scoped>
-.fill-height {
-  height: 100vh;
+
+<style lang="scss">
+.RdYlGn {
+  padding-left: 5em;
+}
+
+.day {
+  fill: $heatmap-bg;
+  stroke: $heatmap-stroke;
+}
+
+.month {
+  fill: none;
+  stroke: $heatmap-bg;
+  stroke-width: 4px;
+}
+.year-title {
+  font-size: 1.5em;
+}
+
+/* color ranges */
+.RdYlGn .q0-11 {
+  fill: $heatmap-r0;
+}
+.RdYlGn .q1-11 {
+  fill: $heatmap-r1;
+}
+.RdYlGn .q2-11 {
+  fill: $heatmap-r2;
+}
+.RdYlGn .q3-11 {
+  fill: $heatmap-r3;
+}
+.RdYlGn .q4-11 {
+  fill: $heatmap-r4;
+}
+.RdYlGn .q5-11 {
+  fill: $heatmap-r5;
+}
+.RdYlGn .q6-11 {
+  fill: $heatmap-r6;
+}
+.RdYlGn .q7-11 {
+  fill: $heatmap-r7;
+}
+.RdYlGn .q8-11 {
+  fill: $heatmap-r8;
+}
+.RdYlGn .q9-11 {
+  fill: $heatmap-r9;
 }
 </style>

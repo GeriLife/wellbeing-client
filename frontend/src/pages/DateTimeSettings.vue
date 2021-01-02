0ba876f68e5222c27cc91fa38070da2becf96978
Shailee Mehta <mehtashailee21@gmail.com>
2021-01-02 13:00:00 +0530

    adding date time page

---
+++ frontend/src/pages/DateTimeSettings.vue
@@ -0,0 +1,13 @@
+<template>
+  <date-time-settings />
+</template>
+
+<script>
+import DateTimeSettings from "src/features/date-time-settings/index.vue";
+
+export default {
+  components: {
+    DateTimeSettings,
+  },
+};
+</script>

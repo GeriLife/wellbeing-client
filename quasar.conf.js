bed3fad24f5879539106c93ebfa7fc39cc316def
Brylie Christopher Oxley <brylie@amble.fi>
2021-04-04 14:02:12 +0300

    Remove port 3000 check

--- quasar.conf.js
+++ quasar.conf.js
@@ -34,7 +34,6 @@ module.exports = function() {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: "hash", // available values: 'hash', 'history'
-      env: require("./src/boot/env-parser")(),
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)

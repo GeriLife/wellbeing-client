7cf8f343b1532dd97db85e79597069e06595c563
Brylie Christopher Oxley <brylie@amble.fi>
2021-04-04 13:30:46 +0300

    Set API base url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import { getCookie } from "src/services/cookies";
 import { checkIfLoggedIn } from "src/services/login.js";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+  "baseURL": "api",
+});
 
 export default () => {
   axiosInstance.interceptors.request.use(

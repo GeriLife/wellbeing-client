3ae631a757cadcf94e71cf36de3c8a22ac026801
Shailee Mehta <mehtashailee21@gmail.com>
2021-04-10 23:42:43 +0530

    35 proxing methods apis to 3000 route in dev

--- quasar.conf.js
+++ quasar.conf.js
@@ -69,6 +69,10 @@ module.exports = function() {
           pathRewrite: {
             "^/api": ""
           }
+        },
+        "/methods": {
+          target: "http://localhost:3000",
+          changeOrigin: true
         }
       }
     },

db235c7696c8550e6b80f037e88079b6c5cc4b70
Shailee Mehta <mehtashailee21@gmail.com>
2021-04-16 21:45:45 +0530

    25 changing navigation bar UI. Adding fixing for smaller screen

--- src/components/GeriLifeTopbar.vue
+++ src/components/GeriLifeTopbar.vue
@@ -1,66 +1,35 @@
 <template>
-  <q-toolbar class="bg-grey-4">
-    <div class="row" style="width: 100%">
-      <q-toolbar-title
-        @click="$route.path !== '/' && $router.push({ path: '/' })"
-        class="cursor-pointer col-md-2"
-      >
-        <q-img
-          width="30px"
-          height="30px"
-          src="~assets/gerilife-logo.png"
-          alt=""
-        />
-        <span class="text-grey-7"> GeriLife<sup>®</sup></span>
-      </q-toolbar-title>
-      <div v-if="!!getCookie('token')" class="col-12 col-md-5">
-        <q-btn
-          @click="$router.push({ path: '/residents' })"
-          class="text-grey-7"
-          :size="isSmOrAbove ? 'md' : 'xs'"
-          flat
-          icon="fa fa-users"
+  <div>
+    <q-toolbar class="bg-grey-4">
+      <div class="row no-wrap" style="width: 100%">
+        <q-toolbar-title
+          @click="$route.path !== '/' && $router.push({ path: '/' })"
+          class="cursor-pointer col-auto"
         >
-          <span class="q-ml-sm">{{
-            $i18n.t("mainLayoutNavbar-residentsLink")
-          }}</span>
-        </q-btn>
-        <q-btn
-          @click="$router.push({ path: '/activities' })"
-          class="text-grey-7"
-          flat
-          :size="isSmOrAbove ? 'md' : 'xs'"
-          icon="fa fa-heartbeat"
-        >
-          <span class="q-ml-sm">{{
-            $i18n.t("mainLayoutNavbar-activitesLink")
-          }}</span>
-        </q-btn>
-        <q-btn
-          @click="$router.push({ path: '/homes' })"
-          class="text-grey-7"
-          flat
-          :size="isSmOrAbove ? 'md' : 'xs'"
-          icon="fa fa-home"
-        >
-          <span class="q-ml-sm">{{
-            $i18n.t("mainLayoutNavbar-homesLink")
-          }}</span>
-        </q-btn>
-        <q-btn
-          @click="$router.push({ path: '/report' })"
-          class="text-grey-7"
-          flat
-          :size="isSmOrAbove ? 'md' : 'xs'"
-          icon="fa fa-chart-line"
-        >
-          <span class="q-ml-sm">{{
-            $i18n.t("mainLayoutNavbar-reportLink")
-          }}</span>
-        </q-btn>
+          <q-img
+            width="30px"
+            height="30px"
+            src="~assets/gerilife-logo.png"
+            alt=""
+          />
+          <span class="text-grey-7"> GeriLife<sup>®</sup></span>
+        </q-toolbar-title>
+        <div class="col-8">
+          <div v-if="!!getCookie('token') && isMdOrAbove">
+            <q-btn
+              v-for="(menuItem, index) in menu"
+              :key="index"
+              class="text-grey-7"
+              flat
+              :icon="menuItem.icon"
+              @click="clicked(menuItem, index)"
+            >
+              <span class="q-ml-sm">{{ menuItem.title }}</span>
+            </q-btn>
+          </div>
+        </div>
       </div>
-      <q-space />
-      <div class="col-3 col-md-1">
+      <div style="max-width: 150px">
         <q-select
           outlined
           dense
@@ -72,43 +41,59 @@
           :options="languageOptions"
         />
       </div>
-      <div class="col-9 col-md-4" v-if="!!getCookie('token')">
-        <q-btn
-          @click="logoutAndRedirect"
-          :size="isSmOrAbove ? 'md' : 'xs'"
-          outline
-          class="float-right flat text-black bg-white"
-        >
-          {{ $i18n.t("logout-logoutButton") }}
-        </q-btn>
 
-        <q-btn
-          @click="$router.push({ path: '/settings' })"
-          class="float-right text-grey-7"
-          flat
-          :size="isSmOrAbove ? 'md' : 'xs'"
-          icon="fa fa-cog"
-        >
-          <span class="q-ml-sm">{{
-            $i18n.t("mainLayoutNavbar-settingsLink")
-          }}</span>
-        </q-btn>
-
-        <q-btn
-          @click="openProfile = true"
-          class="float-right text-grey-7"
-          flat
-          :size="isSmOrAbove ? 'md' : 'xs'"
-          icon="fa fa-user"
-        >
-          <span class="q-ml-sm">{{
-            $i18n.t("mainLayoutNavbar-profileLink")
-          }}</span>
-        </q-btn>
-      </div>
-    </div>
-    <change-password v-if="openProfile" @close="openProfile = false" />
-  </q-toolbar>
+      <q-btn
+        v-if="!!getCookie('token')"
+        @click="logoutAndRedirect"
+        outline
+        class="q-ml-xs flat text-black bg-white"
+      >
+        {{ $i18n.t("logout-logoutButton") }}
+      </q-btn>
+      <q-btn
+        v-if="!!getCookie('token') && !isMdOrAbove"
+        flat
+        @click="drawerRight = !drawerRight"
+        round
+        dense
+        icon="menu"
+      />
+      <change-password v-if="openProfile" @close="openProfile = false" />
+    </q-toolbar>
+    <q-drawer
+      v-if="!!getCookie('token') && !isMdOrAbove"
+      side="right"
+      v-model="drawerRight"
+      :width="200"
+      bordered
+    >
+      <q-scroll-area class="fit">
+        <q-list>
+          <template v-for="(menuItem, index) in menu">
+            <q-item
+              :class="{
+                'text-black': selected !== menuItem.title,
+                'text-white': selected === menuItem.title,
+              }"
+              :key="index"
+              clickable
+              @click="clicked(menuItem, index)"
+              active-class="bg-primary"
+              :active="menuItem.title === selected"
+            >
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.title }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+  </div>
 </template>
 
 <script>
@@ -124,21 +109,50 @@ export default {
   data() {
     return {
       getCookie,
+      drawerRight: false,
+      selected: null,
       openProfile: false,
       languageOptions: [
         { label: "English", value: "en" },
         { label: "Suomi", value: "fi" },
       ],
+      menu: [
+        {
+          icon: "fa fa-users",
+          title: this.$i18n.t("mainLayoutNavbar-residentsLink"),
+          route: "/residents",
+        },
+        {
+          icon: "fa fa-heartbeat",
+          title: this.$i18n.t("mainLayoutNavbar-activitesLink"),
+          route: "/activities",
+        },
+        {
+          icon: "fa fa-home",
+          title: this.$i18n.t("mainLayoutNavbar-homesLink"),
+          route: "/homes",
+        },
+        {
+          icon: "fa fa-chart-line",
+          title: this.$i18n.t("mainLayoutNavbar-reportLink"),
+          route: "/report",
+        },
+        {
+          icon: "fa fa-cog",
+          title: this.$i18n.t("mainLayoutNavbar-settingsLink"),
+          route: "/settings",
+        },
+        {
+          icon: "fa fa-user",
+          title: this.$i18n.t("mainLayoutNavbar-profileLink"),
+          isProfileLink: true,
+        },
+      ],
     };
   },
   computed: {
-    isSmOrAbove() {
-      return (
-        this.$q.screen.sm ||
-        this.$q.screen.md ||
-        this.$q.screen.lg ||
-        this.$q.screen.xl
-      );
+    isMdOrAbove() {
+      return this.$q.screen.md || this.$q.screen.lg || this.$q.screen.xl;
     },
   },
 
@@ -152,9 +166,20 @@ export default {
       }
     },
 
+    clicked(menuItem) {
+      this.selected = menuItem.title;
+      if (menuItem.isProfileLink) {
+        this.openProfile = true;
+      } else {
+        this.$router.push({ path: menuItem.route });
+      }
+    },
+
     changeLanguage(selected) {
       this.$i18n.locale = selected.value;
       localStorage.setItem("locale", selected.value);
+
+      this.$router.go();
     },
   },
 };
